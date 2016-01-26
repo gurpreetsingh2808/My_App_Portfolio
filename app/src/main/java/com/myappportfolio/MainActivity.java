5eb938f523183ee1f19dd55bfab54b86ba5b7ed1
gurpreetsingh2808 <gurpreetsingh2808@gmail.com>
2016-01-26 20:12:10 +0530

    Update MainActivity.java

--- app/src/main/java/com/myappportfolio/MainActivity.java
+++ app/src/main/java/com/myappportfolio/MainActivity.java
@@ -1,53 +1,35 @@
 package com.myappportfolio;
 
-import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
 import android.view.View;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button buttonSpotify = (Button)findViewById(R.id.button);
-        Button buttonScores = (Button)findViewById(R.id.button);
-        Button buttonLibrary = (Button)findViewById(R.id.button);
-        Button buttonBuild = (Button)findViewById(R.id.button);
-        Button buttonXYZ = (Button)findViewById(R.id.button);
-        Button buttonCapstone = (Button)findViewById(R.id.button);
-
-        buttonSpotify.setOnClickListener(this);
-        buttonScores.setOnClickListener(this);
-        buttonLibrary.setOnClickListener(this);
-        buttonBuild.setOnClickListener(this);
-        buttonXYZ.setOnClickListener(this);
-        buttonCapstone.setOnClickListener(this);
-
     }
 
-    @Override
-    public void onClick(View v) {
-        if(v.getId() == R.id.button) {
+    public void displayToast(View v) {
+        if(v.getId() == R.id.buttonSpotify) {
             Toast.makeText(MainActivity.this, "This button will launch Spotify Streamer app", Toast.LENGTH_SHORT).show();
         }
-        else if(v.getId() == R.id.button2) {
+        else if(v.getId() == R.id.buttonScores) {
             Toast.makeText(MainActivity.this, "This button will launch Scores app", Toast.LENGTH_SHORT).show();
         }
-        else if(v.getId() == R.id.button3) {
+        else if(v.getId() == R.id.buttonLibrary) {
             Toast.makeText(MainActivity.this, "This button will launch Library app", Toast.LENGTH_SHORT).show();
         }
-        else if(v.getId() == R.id.button4) {
+        else if(v.getId() == R.id.buttonBuild) {
             Toast.makeText(MainActivity.this, "This button will launch SBuild it bigger app", Toast.LENGTH_SHORT).show();
         }
-        else if(v.getId() == R.id.button5) {
+        else if(v.getId() == R.id.buttonXYZ) {
             Toast.makeText(MainActivity.this, "This button will launch XYZ Reader app", Toast.LENGTH_SHORT).show();
         }
-        else {
+        else if(v.getId() == R.id.buttonCapstone) {
             Toast.makeText(MainActivity.this, "This button will launch Capstone app", Toast.LENGTH_SHORT).show();
         }
     }
